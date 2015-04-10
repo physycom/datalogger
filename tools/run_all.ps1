f3162436052e33adb70baeb5d03b368fa932aaa6
MrRomanMaccaruni <rommacc@yahoo.it>
2015-04-10 15:44:45 +0200

    adding multi-purposes ps scripts

---
+++ tools/run_all.ps1
@@ -0,0 +1,19 @@
+﻿$scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$parentPath = (get-item $scriptPath).parent.FullName
+
+$READER = $parentPath + "\Release\serial_reader.exe"
+$VIEWER = $parentPath + "\Release\serial_viewer.exe"
+
+$META_PARAM = "-t 5 -b 115200 -p COM27"
+$TEXA_PARAM = "-t 3 -b 115200 -p COM21"
+$OCTO_PARAM = "-t 7 -b 115200 -p COM17"
+$MM_2_PARAM = "-t 9 -b 115200 -p COM20"
+
+Start-Process $VIEWER
+
+Start-Sleep -m 500 # wait for 500 milliseconds
+
+Start-Process $READER $META_PARAM
+#Start-Process $READER $TEXA_PARAM
+Start-Process $READER $OCTO_PARAM
+Start-Process $READER $MM_2_PARAM
