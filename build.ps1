c8d9faa64612ea09c6ef837c52d4b921d4e23801
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-19 19:22:14 +0100

    update to the latest sysconfig

--- build.ps1
+++ build.ps1
@@ -1,4 +1,11 @@
+#!/usr/bin/env powershell
+
 mkdir -Force build
 cd build
-cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
+
+#statically linked
+cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
+
+cmake --build . --config Release
+
 cd ..
